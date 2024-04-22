5cbc673bfc699e6e606bb4496d8abccbc2dd4c83
simofleanta <simo.fleanta@gmail.com>
2024-04-22 14:09:44 +0300

    Create first_class.cs

---
+++ first_class.cs
@@ -0,0 +1,24 @@
+//C# is an object-oriented program. In object-oriented programming(OOP), we solve complex problems by dividing them into objects.
+//To work with objects, we need to perform the following activities:
+
+//create a class
+//create objects from the class
+
+// first class 
+
+class program{
+
+}
+
+// a class can contain  * variables to store data * methods to perform the tasks 
+
+class dog{
+
+    //field
+    string breed
+
+    //method
+    public void bark() {
+
+    }
+}
