be6abed5c496583222733a49dafdb85a20b46863
simofleanta <simo.fleanta@gmail.com>
2024-04-30 10:18:51 +0300

    Create doublevar.cs

---
+++ doublevar.cs
@@ -0,0 +1,13 @@
+namespace apps;
+
+public class values_to_variables
+{
+    class values_to_variables1
+    {
+        public void Main(string[] args)
+        {
+            double x=50.05;
+            Console.WriteLine(x);
+        }
+    }
+}
