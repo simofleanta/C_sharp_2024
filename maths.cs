57f73483344019819df8612fca70bd9e059a0fb3
simofleanta <simo.fleanta@gmail.com>
2024-04-30 10:19:44 +0300

    Create maths.cs

---
+++ maths.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Program
+{
+    class Maths
+    {
+        static void Main(string[] args)
+        {
+            int x=3;
+            int y=4;
+            int z=(x+y);
+            Console.WriteLine(z);
+        }
+    }
+}
