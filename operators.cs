3a37d2b117484116e92150b92d90a4527b0f40bf
simofleanta <simo.fleanta@gmail.com>
2024-04-30 10:24:40 +0300

    Create operators.cs

---
+++ operators.cs
@@ -0,0 +1,9 @@
+namespace app2;
+
+public class Operators1
+{
+    static void Main(string[]arggs)
+    {
+        Console.WriteLine("in 2+3, + is an operator that is used to carry out addition operation, while 2 and 3 are operands");
+    }
+}
