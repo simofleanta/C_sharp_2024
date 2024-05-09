f04520d8b5c1f180c447b1d06aa22e3abcebafd3
simofleanta <simo.fleanta@gmail.com>
2024-05-09 15:06:13 +0300

    Create increment.cs

---
+++ increment.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare steps variable
+            int steps = 0;
+
+
+            Console.WriteLine(steps);
+
+
+
+            // Two steps forward 
+            steps += 2;
+
+
+            // One step back 
+            steps--;
+
+
+            // Print result to the console
+            Console.WriteLine(steps);
+        }
+    }
+}
