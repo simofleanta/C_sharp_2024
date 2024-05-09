3a5c8246a952d18ffe7c7e5f85ce999b8a474e3e
simofleanta <simo.fleanta@gmail.com>
2024-05-09 17:16:19 +0300

    Update Math.cs

--- Math.cs
+++ Math.cs
@@ -27,11 +27,6 @@ Which value gets printed to the console?
 Do the same process to variable numberTwo and save this value to a new double variable numberTwoSqrt. */
 
 
-
-
-
-
-
       using System;
 
 namespace LowestNumber
@@ -52,6 +47,44 @@ Console.WriteLine(numberOneSqrt);
 
       // Use built-in methods and save to variable 
 double numberTwoSqrt = Math.Floor(Math.Sqrt(numberTwo));
+Console.WriteLine(numberTwoSqrt);
+
+      // Print the lowest number
+      
+      Console.WriteLine(Math.Min(numberOneSqrt,numberTwoSqrt));
+
+
+    }
+  }
+}
+
+
+      /* Did NaN get printed to the console? NaN stands for “Not a Number” in C#. So what happened?
+
+The built-in method Math.Sqrt() can only take a positive number as a value, but the value of numberTwo is negative. But we can fix it. The method Math.Abs() will find the absolute value of a number. A good reminder that when we use built-in methods, to check the documentation so we know how to use them!
+
+Inside of the Math.Sqrt() method, add the Math.Abs() method, so it takes the variable numberTwo as a value. Re-run the code and see what gets printed to the console this time! */
+
+using System;
+
+namespace LowestNumber
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Starting variables 
+      int numberOne = 12932;
+      int numberTwo = -2828472;
+
+      // Use built-in methods and save to variable 
+double numberOneSqrt=Math.Floor(Math.Sqrt(numberOne));
+Console.WriteLine(numberOneSqrt);
+
+
+
+      // Use built-in methods and save to variable 
+double numberTwoSqrt = Math.Floor(Math.Sqrt(Math.Abs(numberTwo)));
 Console.WriteLine(numberTwoSqrt);
 
       // Print the lowest number
