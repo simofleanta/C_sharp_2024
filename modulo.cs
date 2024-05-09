7dfa60c6dce52ab82825d10b629607555ac13dd1
simofleanta <simo.fleanta@gmail.com>
2024-05-09 15:31:09 +0300

    Create modulo.cs

---
+++ modulo.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace ClassTeams
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Number of students
+      int students=18;
+      Console.WriteLine(students);
+
+
+      // Number of students in a group
+int  groupSize=3;
+ Console.WriteLine(students % groupSize);
+
+      // Does groupSize go evenly into students?
+
+
+
+    }
+  }
+}
