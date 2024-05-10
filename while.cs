db8ef4ac96467f6cf6ea4393d19b713088a58d16
simofleanta <simo.fleanta@gmail.com>
2024-05-10 13:13:40 +0300

    while.cs

---
+++ while.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 20;
+            while (day > 0)
+            {
+                Console.WriteLine(day);
+                day--;
+                break;
+            }
+        }
+    }
+}
+
+
