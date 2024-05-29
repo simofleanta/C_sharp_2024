2803c7d5fb1b98879d13883388b1d31b6833416d
simofleanta <simo.fleanta@gmail.com>
2024-05-29 12:14:32 +0300

    Create returnF_naminfF.cs

---
+++ returnF_naminfF.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp39
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Family(child1: "Sam", child2: "John");
+
+        }
+        static int fam_age (int x)
+        { 
+            return x + 10; 
+        }
+        static void Family(string child1, string child2) 
+        {
+            Console.WriteLine($"The youngest one is: {child1}");
+            Console.WriteLine($"all together they are: {fam_age(5)}");
+        }
+    }
+}
+
