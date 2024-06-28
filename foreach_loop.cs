d5017fd99adc24d1a754e41a6f6ab4c1bce48d8e
simofleanta <simo.fleanta@gmail.com>
2024-06-28 11:09:35 +0300

    Update foreach_loop.cs

--- foreach_loop.cs
+++ foreach_loop.cs
@@ -18,3 +18,10 @@ namespace forwach_loop    //program that displays days of the week use foreach f
         }
     }
 }
+
+
+/* code steps:                                   | /* programatic/logical thinking for program that runs days of the week
+* pass variable                                  |  * perform foreach loop 
+* perform foreach loop                           |  * if you want to extract a day from the list/array use if to trigguer true so if day == Tue then print Tuesday else break 
+* print with console writeline                   |
+*/
