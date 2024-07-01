b8695a854c9aeb5681bbdbca565d5be52823da63
simofleanta <simo.fleanta@gmail.com>
2024-07-01 11:51:50 +0300

    Create extractname_logic.cs

---
+++ extractname_logic.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Linq;
+namespace ConsoleApp49
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            { // Task: extract the first and the lastname from the names/records john doe and adam smith
+             // logic used :
+              /* used for each loop to display the names
+               * used split function to extract fn
+               * used Substring function to extract last name
+               * */
+
+                string[] name = { "John Doe", "Adam Smith" };
+                foreach (string i in name)
+                {
+                //print the names 
+                Console.WriteLine(i);
+                //extract first names
+                Console.WriteLine(i.Split(' ')[0]);
+                // extract ln              
+                Console.WriteLine(i.Substring(i.IndexOf(' ')));
+              
+            }
+
+
+
+
+        }
+    }
+
+}
+    
+
