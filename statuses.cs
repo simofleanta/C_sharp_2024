b9a77261531cdcd3cfb1b5350530a78a4c7655ef
simofleanta <simo.fleanta@gmail.com>
2024-07-01 16:02:50 +0300

    Create statuses.cs

---
+++ statuses.cs
@@ -0,0 +1,18 @@
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp52
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// all companies in finalied 
+            string amazon = "finalized";
+            string pwc = "dev";
+            string deloitte = "in progress";
+            string solaris = "finalized ";
+            
+        }
+    }
+}
