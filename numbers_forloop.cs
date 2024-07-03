a46c3626c86d97ffc5c5f1436483b27fdfd953f0
simofleanta <simo.fleanta@gmail.com>
2024-07-03 12:25:59 +0300

    Create numbers_forloop.cs

---
+++ numbers_forloop.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp54
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 15, 10, 24, 4, 5 };
+            for (int i = 0; i < numbers.Length; i++)  
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
