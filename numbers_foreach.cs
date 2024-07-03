725cec719d3caab581f64533bce385abc0e3f818
simofleanta <simo.fleanta@gmail.com>
2024-07-03 12:23:13 +0300

    Create numbers_foreach.cs

---
+++ numbers_foreach.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp54
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 15, 10, 24, 4, 5 };
+            foreach (int i in numbers) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
