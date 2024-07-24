9a5540bfb0d674ad080fb8ba8a640b0c0a9b6f85
simofleanta <simo.fleanta@gmail.com>
2024-07-24 11:13:59 +0300

    Method_to_hello.cs

---
+++ method_to_hello.cs
@@ -0,0 +1,17 @@
+using System;
+namespace Fn
+{
+    class Program_f
+    {
+        static void hello(string name) 
+        {
+            Console.WriteLine("Hello "+ name);
+        }
+
+        static void Main()
+        {
+            hello("John");
+        }
+
+    }
+}
